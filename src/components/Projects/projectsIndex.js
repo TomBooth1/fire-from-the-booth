a0e02ebbacc190994c59d403076062df27dce5d9
TomBooth <thomas.alexander.booth@gmail.com>
2022-07-12 22:46:08 +0100

    added projects banner and first icon

--- src/components/Projects/projectsIndex.js
+++ src/components/Projects/projectsIndex.js
@@ -4,8 +4,23 @@ function Projects () {
 
     return (
         <section className="projects" id="projects">
-<h1>Projects</h1>
+            <div className="projects-banner">
+                <div className="projects-title">
+                    <h1 className="section-title">Projects</h1>
+                </div>
+            </div>
 
+            <div className="project-icons">
+            <div className="project-icon-background">
+            <img className="react-logo" src={process.env.PUBLIC_URL + '/react-logo-white.png'} alt="react logo" />
+            </div>
+            </div>
+            <div className="project-titles">
+            <div className="project-title-container">
+                <h1 className="project-icon-title">'Make Your Mon'</h1>
+                <p className="project-icon-description"> A React Pokemon Creator</p>
+            </div>
+            </div>
         </section>
     )
 }
