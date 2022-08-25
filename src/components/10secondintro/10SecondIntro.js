0c86266106ba845e25fcf2ce3494a7f2c2f76d7e
TomBooth <thomas.alexander.booth@gmail.com>
2022-08-25 14:13:34 +0100

    centered all visibile project icon containers still the logo is off?????

--- src/components/10secondintro/10SecondIntro.js
+++ src/components/10secondintro/10SecondIntro.js
@@ -6,7 +6,7 @@ function Intro () {
             <img className="intro-icon" src={process.env.PUBLIC_URL + '/exclamation-icon-blue.png'} alt="Exclamation mark in blue circle" />
             <div className="background-style-intro">
            <p className="intro-paragraph">
-           I used to be a Backstage Technicain and Production manager. Now im a Backstage Developer managing code. <br/><br/>Below is Some of my Work.
+           I used to be a Backstage Technician and Production manager. Now I'm a Backstage Developer managing code. <br/><br/>Below is Some of my Work.
             </p>
            </div>
         </section>
